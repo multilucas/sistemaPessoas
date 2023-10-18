dab996b29062da14e4a95fc854d2a968f5434140
multilucas <lucaslopes198@gmail.com>
2023-10-17 22:57:09 -0300

    second part of challenge

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Pessoa meuFuncionario = new Pessoa();
+        Pessoa meuFuncionario = new Pessoa("Lucas",25,1.79,89.6,(byte) 'M',"874.545.632-12","Josefina Maria das Graças");
         meuFuncionario.setIdade(25);
         meuFuncionario.setNome("Lucas");
+        meuFuncionario.setAltura(1.8);
+        meuFuncionario.setCpf("985.546.435-34");
+        meuFuncionario.setPeso(79.7);
+        meuFuncionario.setGenero((byte) 'M');
+        meuFuncionario.setNomeDaMae("Josefina Maria das Graças");
+
 
         System.out.println(meuFuncionario.getNome());
         System.out.println(meuFuncionario.getIdade());
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,13 +1,68 @@
 public class Pessoa {
     private String nome;
     private int idade;
+    private double altura;
+    private double peso;
+    private byte genero;
+    private String cpf;
+    private String nomeDaMae;
 
     public String getNome() {
         return nome;
     }
 
+    public Pessoa(String nome, int idade, double altura, double peso, byte genero, String cpf, String nomeDaMae) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.genero = genero;
+        this.cpf = cpf;
+        this.nomeDaMae = nomeDaMae;
+    }
+
     public int getIdade() {
         return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public byte getGenero() {
+        return genero;
+    }
+
+    public void setGenero(byte genero) {
+        this.genero = genero;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNomeDaMae() {
+        return nomeDaMae;
+    }
+
+    public void setNomeDaMae(String nomeDaMae) {
+        this.nomeDaMae = nomeDaMae;
     }
 
     public void setNome(String nome) {
